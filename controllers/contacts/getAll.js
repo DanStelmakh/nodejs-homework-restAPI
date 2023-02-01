390ee14022dea025e09dbe226a487736046e100b
DanStelmakh <danylo.stelmakh@gmail.com>
2023-02-01 13:13:12 +0200

    add filter by query, add pagination

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,15 +3,33 @@ const createError = require("http-errors");
 
 const getAll = async (req, res, next) => {
   try {
+    const { page, limit, favorite } = req.query;
+
+    const skip = (page - 1) * limit;
+
     const { _id } = req.user;
 
+    // filter by query "favorite"
+    await Contact.aggregate([
+      {
+        $match: {
+          favorite,
+        },
+      },
+    ]);
+
     const allContacts = await Contact.find(
-      { owner: _id },
-      "-createdAt -updatedAt"
+      { owner: _id, favorite },
+      "-createdAt -updatedAt",
+      {
+        skip,
+        limit: +limit,
+      }
     ).populate("owner", "_id email subscription");
     if (!allContacts.length) {
       throw createError(404, `No contacts, please try later.`);
     }
+
     res.json({
       status: "Success",
       code: 200,
